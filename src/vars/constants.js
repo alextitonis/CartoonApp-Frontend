0bcb236e5b147c09b8a634b0c1d3f126cfc05bc0
alextitonis <45359358+alextitonis@users.noreply.github.com>
2024-03-18 17:49:19 +0300

    Update constants.js

--- src/vars/constants.js
+++ src/vars/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://127.0.0.1:3000";// "https://fairytalechat-d5c202ca0b62.herokuapp.com"; // "http://34.16.171.165:3000";
+export const API_URL = "https://fairytalechat-d5c202ca0b62.herokuapp.com"; // "http://34.16.171.165:3000";
 export const FILESERVER_URL =
   "https://fileserverfairytalechat-33d4dd18703c.herokuapp.com";
